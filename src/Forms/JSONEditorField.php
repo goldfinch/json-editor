11fc96b5521e6d8730ac30279abf27fde49e9ffd
Art Karasev <as.karasyov@gmail.com>
2023-10-19 14:56:23 +1300

    tweaks

--- src/Forms/JSONEditorField.php
+++ src/Forms/JSONEditorField.php
@@ -9,9 +9,24 @@ class JSONEditorField extends TextField
 {
     public function __construct($name, $title = null, $value = '{}', $form = null, $schema = '{}')
     {
+        if (!$value)
+        {
+            $value = '{}';
+        }
+
+        if (!$schema)
+        {
+            $schema = '{}';
+        }
+
         parent::__construct($name, $title, $value);
+        // parent::__construct($name, $title, $value, '', $form);
 
         Requirements::css('goldfinch/json-editor:client/dist/resources/assets/json-editor-style.css');
         Requirements::javascript('goldfinch/json-editor:client/dist/resources/assets/json-editor.js');
+
+        $this->addExtraClass('jsoneditorfield');
+        $this->setInputType('hidden');
+        $this->setAttribute('data-field-schema', $schema);
     }
 }
---
+++ templates/Goldfinch/JSONEditor/Forms/JsonEditorField.ss
@@ -0,0 +1,3 @@
+<div class="json-editor" id="Form_ItemEditForm_{$Name}_Editor"></div>
+<input $AttributesHTML />
+<span id="valid_indicator"></span>
