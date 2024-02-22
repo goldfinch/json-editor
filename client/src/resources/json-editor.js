2f1c6256da73dc03639b0b5ccc591f6b2373ab06
Art Karasev <2291826+goldfinch@users.noreply.github.com>
2024-02-22 14:56:26 +1300

    WIP

--- client/src/resources/json-editor.js
+++ client/src/resources/json-editor.js
@@ -9,7 +9,7 @@ window.jsoneditorSetDefaultValue = (e) => {
     const editorist = window.jsoneditor[my_id];
     const schema = window.jsoneditorschema[my_id];
     if (editorist) {
-      console.log(schema.properties);
+      // console.log(schema.properties);
       editorist.setValue(schema.properties);
     }
   }
@@ -54,7 +54,19 @@ window.jsoneditorSetDefaultValue = (e) => {
 
           const editor = new JSONEditor(editorist, options);
 
+          if (!window.jsoneditor) {
+            window.jsoneditor = [];
+            window.jsoneditorschema = [];
+            window.jsoneditorfieldoptions = [];
+          }
+
+          window.jsoneditor[my_id] = editor;
+
           editor.on('ready', () => {
+            if (fieldOptions) {
+              window.jsoneditorfieldoptions[my_id] = fieldOptions;
+            }
+
             if (fieldOptions && fieldOptions.set_default_button) {
               if (
                 schema &&
@@ -62,14 +74,7 @@ window.jsoneditorSetDefaultValue = (e) => {
                 schema != '{}' &&
                 JSON.parse(schema).type
               ) {
-                if (!window.jsoneditor) {
-                  window.jsoneditor = [];
-                  window.jsoneditorschema = [];
-                  window.jsoneditorfieldoptions = [];
-                }
-                window.jsoneditor[my_id] = editor;
                 window.jsoneditorschema[my_id] = JSON.parse(schema);
-                window.jsoneditorfieldoptions[my_id] = fieldOptions;
 
                 jQuery(`#${my_id}`)
                   .parent()
